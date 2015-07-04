3aaa76bca2d2c8522342f28d50ec63b1a8a8522d
Tom Gillooly <tomgillooly@gmail.com>
2015-07-04 21:18:52 +1200

    Boids instantiated outside boids.py

--- boids.py
+++ boids.py
@@ -26,120 +26,61 @@ from random import randrange, random, seed
 import time
 import graphics
 from pygame.math import Vector2
+import pygame.sprite
 
 import operator
 import math
 
-class Boid(object):
-    def __init__(self, x, y):
-        pass
+class Boid(pygame.sprite.Sprite):
+    def __init__(self, pos, vel):
+        super(Boid, self).__init__()
+        self.position = pos
+        self.velocity = vel
 
-class Boids:
-    def __init__(self, numboids = 10, dimensions={'x':640, 'y':480}):     #class constructor with default parameters filled
-        #class constants
+    def update(self, dt, mob, MINX, MAXX, MINY, MAXY):
+        if self.position.x < MINX:
+            self.position.x = MINX
+            self.velocity.x = -self.velocity.x
 
-        #the next six lines define the boundaries of the torus
-        """
-        torus:  donut shaped space, i.e. infinite
-        effect: boids flying out of bounds appear at the opposite side
-        note:   cartesian matrices don't handle toruses very well, but I couldn't
-                figure out a better way to keep the flock in view.
-        """
-        seed(time.time())
+        if self.position.x > MAXX:
+            self.position.x = MAXX
+            self.velocity.x = -self.velocity.x
 
-        self.MINX = 0     #left
-        self.MINY = 0   #bottom
-        self.MAXX = dimensions['x']           #right
-        self.MAXY = dimensions['y']          #top
+        if self.position.y < MINY:
+            self.position.y = MINY
+            self.velocity.y = -self.velocity.y
 
-        self.RADIUS = 1                 #radius of a boid.  I wimped and used spheres.
-        self.NEARBY = self.RADIUS * 5   #the 'halo' of space around each boid
-        self.FACTOR = .95               #the amount of movement to the perceived flock center
-        self.NEGFACTOR = self.FACTOR * -1.0 #same thing, only negative
+        if self.position.y > MAXY:
+            self.position.y = MAXY
+            self.velocity.y = -self.velocity.y
 
-        self.NUMBOIDS = numboids        #the number of boids in the flock
+        v1 = self.rule1(mob)              #get the vector for rule 1
+        v2 = self.rule2(mob)              #get the vector for rule 2
+        v3 = self.rule3(mob)              #get the vector for rule 3
 
-        self.boidflock = []             #empty list of boids
-        self.velocities = []
-        self.DT = 0.02                  #delay time between snapshots
+        boidvelocity = v1 + v2 + v3  #accumulate the rules vector results
+        if (boidvelocity.length() != 0):
+            boidvelocity.scale_to_length(20)
 
-        self.initializePositions()      #create a space with boids
+        self.position += dt * boidvelocity
+        self.velocity = boidvelocity #move the boid
 
-    def initializePositions(self):
-        #wire frame of space
-        leftBottom = graphics.curve([(self.MINX, self.MINY), (self.MINX, self.MINY)], graphics.WHITE)
-        leftTop = graphics.curve([(self.MINX, self.MAXY), (self.MINX, self.MAXY)], graphics.WHITE)
-        rightBottom = graphics.curve([(self.MAXX, self.MINY), (self.MAXX, self.MINY)], graphics.WHITE)
-        rightTop = graphics.curve([(self.MAXX, self.MAXY), (self.MAXX, self.MAXY)], graphics.WHITE)
-
-        #splatter a flock in the space randomly
-        for b in range(self.NUMBOIDS):          #for each boid, ...
-            x = randrange(self.MINX, self.MAXX) #random left-right
-            y = randrange(self.MINY, self.MAXY) #random up-down
-
-            #splat a boid, add to flock list
-            velocity = Vector2((random() * 2) - 1, (random() * 2) - 1).normalize() * 10
-            self.boidflock.append((Vector2(x, y), velocity))
-
-    def moveAllBoidsToNewPositions(self, dt):
-        for i, b in enumerate(self.boidflock):
-            #manage boids hitting the torus 'boundaries'
-            if b[0].x < self.MINX:
-                b[0].x = self.MINX
-                b[1].x = -b[1].x
-
-            if b[0].x > self.MAXX:
-                b[0].x = self.MAXX
-                b[1].x = -b[1].x
-
-            if b[0].y < self.MINY:
-                b[0].y = self.MINY
-                b[1].y = -b[1].y
-
-            if b[0].y > self.MAXY:
-                b[0].y = self.MAXY
-                b[1].y = -b[1].y
-
-            collision_radius = 100
-
-            def collision_occurred(m):
-                return math.sqrt(b[0].distance_squared_to(m[0])) <= collision_radius
-
-            without_me = filter(lambda x: x != b, self.boidflock)
-
-            mob = filter(collision_occurred, without_me)
-
-            v1 = self.rule1(b, mob)              #get the vector for rule 1
-            v2 = self.rule2(b, mob)              #get the vector for rule 2
-            v3 = self.rule3(b, mob)              #get the vector for rule 3
-
-            boidvelocity = v1 + v2 + v3  #accumulate the rules vector results
-#            print boidvelocity
-#            old_b = b
-            self.boidflock[i] = (b[0], boidvelocity) #move the boid
-
-
-#       print self.boidflock
-        for i, boid in enumerate(self.boidflock):
-        #   print boid
-           self.boidflock[i] = (boid[0] + boid[1] * dt, boid[1])
-
-    def rule1(self, aboid, mob):    #Rule 1:  boids fly to perceived flock center
+    def rule1(self, mob):    #Rule 1:  boids fly to perceived flock center
         if not mob:
             return Vector2(0, 0)
 
-        pfc = reduce(operator.add, zip(*mob)[0], Vector2(0, 0))
+        pfc = reduce(operator.add, [x.position for x in mob], Vector2(0, 0))
 
         pfc = pfc/len(mob)             #average the pfc
 
-        return (pfc - aboid[0])
+        return (pfc - self.position)
 
-    def rule2(self, aboid, mob):    #Rule 2: boids avoid other boids
+    def rule2(self, mob):    #Rule 2: boids avoid other boids
         if not mob:
             return Vector2(0, 0)
 
         def get_nudge(x):
-            dir = aboid[0] - x[0]
+            dir = self.position - x.position
             mag = 20 - dir.length()
 
             try:
@@ -151,9 +92,9 @@ class Boids:
 
         return reduce(operator.add, nudges, Vector2(0, 0))
 
-    def rule3(self, aboid, mob):    #Rule 3: boids try to match speed of flock
-        pfv = reduce(lambda b, x: b + x[1], mob, Vector2(0.0,0.0))   #pfv: perceived flock velocity
+    def rule3(self, mob):    #Rule 3: boids try to match speed of flock
+        pfv = reduce(lambda b, x: b + x.velocity, mob, Vector2(0.0,0.0))   #pfv: perceived flock velocity
 
-        pfv = (pfv + aboid[1]) / (len(mob) + 1)
+        pfv = (pfv + self.velocity) / (len(mob) + 1)
 
         return pfv
--- main.py
+++ main.py
@@ -1,31 +1,64 @@
 #!/usr/bin/python
+from __future__ import print_function
 import pygame
 import sys
 
 import graphics
 import boids
 
+import math
+
+from random import randrange, random, seed
+
 boid_obj = None
 
+boidflock = None
+
 def start():
-    global boid_obj
+    global boidflock
+
+    seed(time.time())
 
     graphics.init(width=640, height=480)
 
     graphics.bg_colour(graphics.BLACK)
 
-    boid_obj = boids.Boids(dimensions={'x':640,'y':480})
+    #splatter a flock in the space randomly
+    def generate_boid():
+        x = randrange(0, 640) #random left-right
+        y = randrange(0, 480) #random up-down
 
-    map(lambda pos: graphics.circle(pos, 1, graphics.WHITE), zip(*boid_obj.boidflock)[0])
+        position = pygame.math.Vector2(x, y)
+        #splat a boid, add to flock list
+        velocity = pygame.math.Vector2((random() * 2) - 1, (random() * 2) - 1)
+        velocity.scale_to_length(10)
+
+        return boids.Boid(position, velocity)
+
+    boidflock = [generate_boid() for x in range(20)]
+
+    list(map(lambda b: print(b.velocity), boidflock))
+
+    map(lambda b: graphics.circle(b.position, 1, graphics.WHITE), boidflock)
 
     graphics.draw()
 
 def update(dt):
-    boid_obj.moveAllBoidsToNewPositions(dt)
+    def boid_update(b):
+        without_me = filter(lambda x: x != b, boidflock)
+
+        def collision_occurred(m):
+            return math.sqrt(b.position.distance_squared_to(m.position)) <= 100
+
+        mob = filter(collision_occurred, without_me)
+
+        b.update(dt, mob, 0, 640, 0, 480)
+
+    map(boid_update, boidflock)
 
     graphics.bg_colour(graphics.BLACK)
 
-    map(lambda pos: graphics.circle(pos, 1, graphics.WHITE), zip(*boid_obj.boidflock)[0])
+    map(lambda b: graphics.circle(b.position, 1, graphics.WHITE), boidflock)
 
 #    graphics.circle(boid_obj.greenie_pos, 5, graphics.GREEN)
 
@@ -43,7 +76,7 @@ def update_loop(fps):
         last_loop = loop_start
 
         sleep_time = max(0, 1.0 / fps - (time.time() - loop_start))
-#        print sleep_time
+
         time.sleep(sleep_time)
 
 if __name__ == '__main__':
